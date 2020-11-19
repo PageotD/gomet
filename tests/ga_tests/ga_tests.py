c074bf88e2668cf528fe2cb93ad1b48fe00c4baf
PageotD <damien.pageot@gmail.com>
2020-11-19 18:26:34 +0100

    [TST] Genetic Algorithm

--- tests/ga_tests/ga_tests.py
+++ tests/ga_tests/ga_tests.py
@@ -1,8 +1,24 @@
+from numpy.testing import assert_, assert_raises
 import numpy as np
+
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../gomet')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 
+                                                '../../gomet')))
 
 from gomet.genetic_algorithm import GeneticAlgorithm as ga
 
+class TestGeneticAlgorithm:
+    
+    def test_power2_lower(self):
+        assert(ga._power2(self, 240) == 256)
+        
+    def test_power2_higher(self):
+        assert(ga._power2(self, 260) == 512)
+        
+    def test_power2_exact(self):
+        assert(ga._power2(self, 260) == 512)
+        
+if __name__ == "__main__" :
+    np.testing.run_module_suite()
