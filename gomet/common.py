a1e6488f2edacef2e63f030155ecbf69f33a7b5b
PageotD <damien.pageot@gmail.com>
2020-12-05 22:18:46 +0100

    [DEV] Genetic Algorithm

---
+++ gomet/common.py
@@ -0,0 +1,11 @@
+class Candidate:
+    def __init__(self, **kargs):
+        self.__dict__.update(kargs)
+        
+class GBounds:
+    def __init__(self):
+        pass
+    
+class Report:
+    def __init__(self):
+        pass
